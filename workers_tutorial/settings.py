649d22e59c61de38009c83cfa3aeaf6a37eff4b5
fch-yk <yefim_korshever@mail.ru>
2024-03-02 16:30:28 +0300

    Add django_workers logging

--- workers_tutorial/settings.py
+++ workers_tutorial/settings.py
@@ -131,3 +131,23 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django_workers': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+    },
+}
